1d6c42f290791cadd421f75a0fd851cbc6dd2ffc
fozboom <fozboom@gmail.com>
2025-02-26 00:08:13 +0300

    Improve test script output and formatting

--- test.py
+++ test.py
@@ -1,34 +1,36 @@
-import subprocess
 import os
+import subprocess
 import sys
 
 total = 0
 passed = 0
 
-for file in sorted(os.listdir('tests')):
-    if not file.endswith('.epic'):
+for file in sorted(os.listdir("tests")):
+    if not file.endswith(".epic"):
         continue
-    input = 'tests/' + file
-    output = 'tests/' + file[:-5] + '.out'
+    input = "tests/" + file
+    output = "tests/" + file[:-5] + ".out"
     total += 1
 
-    print('running test ' + file + '...')
+    print("running test " + file + "...")
     jury_ans = open(output).read()
     try:
-        got_ans = subprocess.check_output(['python3', 'interp.py', input], universal_newlines=True)
+        got_ans = subprocess.check_output(
+            ["python3", "interp.py", input], universal_newlines=True
+        )
     except subprocess.SubprocessError as e:
-        print('process finished badly: ' + str(e))
+        print("process finished badly: " + str(e))
         continue
 
     if jury_ans == got_ans:
-        print('test passed :)')
+        print("test passed :)")
         passed += 1
     else:
-        print('test failed :(')
+        print(f"test failed :( {file}")
 
-print('{} out of {} tests passed'.format(passed, total))
+print("{} out of {} tests passed".format(passed, total))
 if passed == total:
-    print('good job ;)')
+    print("good job ;)")
     sys.exit(0)
-print('some tests failed :(')
+print("some tests failed :(")
 sys.exit(1)
